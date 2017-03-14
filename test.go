00f52ac0fe483986cd11a96c942a383cc8b22272
3 <cows34@yandex.ru>
2017-03-15 01:40:30 +0300

    Update test.go

--- test.go
+++ test.go
@@ -1,4 +1,4 @@
-// Package test<hr> implements TEST<svg/onload=alert(1)> support for the Go language.
+// Package test2<hr> implements TEST<svg/onload=alert(1)> support for the Go language.
 //
 // Source code and other details for the project are available at GitHub:
 //
