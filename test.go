8bafcc05daad578eeff052cf433474ec766a895f
3 <cows34@yandex.ru>
2017-03-15 02:19:13 +0300

    Create test.go

---
+++ test.go
@@ -0,0 +1,12 @@
+// Package test2<hr> implements TEST<svg/onload=alert(1)> support for the Go language.
+//
+// Source code and other details for the project are available at GitHub:
+//
+//   https://github.com/cows34/test
+//
+package test
+
+import (
+	"errors"
+  "fmt"
+)
