5d29a247265de014b2f8392d0397e1197b95abc6
3 <cows34@yandex.ru>
2017-03-15 01:38:20 +0300

    Create test.go

---
+++ test.go
@@ -0,0 +1,13 @@
+// Package test implements TEST support for the Go language.
+//
+// Source code and other details for the project are available at GitHub:
+//
+//   https://github.com/cows34/test
+//
+package test
+
+import (
+	"errors"
+  "fmt"
+)
+
